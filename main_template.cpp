dc0cd41882e7ad7f2aef096a6eb6410452a05b4a
Vijaykaarti Sundarapandiyan <vijaykaarti4@gmail.com>
2023-04-26 17:23:57 -0400

    Update main_template.cpp

--- main_template.cpp
+++ main_template.cpp
@@ -15,11 +15,11 @@ const double PI = 3.1415926535897932384626433832795;
 inline void fast_cin() {ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);}
 string to_upper(string a) { transform(a.begin(), a.end(), a.begin(), ::toupper); return a; }
 string to_lower(string a) { transform(a.begin(), a.end(), a.begin(), ::tolower); return a; }
-template<typename T> void print(T arr) { for(auto element: arr) {cout << element << " ";} cout << '\n';}
-template <typename T> void print_v(const vector<T>& v) {cout << "{"; for (const auto& x : v) {cout << x << ",";} cout << "}";}
-template<typename K, typename V, typename Compare = less<K>> void print_m(const map<K, V, Compare>& m) { for (const auto& [k, v] : m) { cout << k << " : " << v << " "; } cout << '\n'; }
-template<typename K, typename V, typename Hash = hash<K>, typename KeyEqual = equal_to<K>> void print_m(const unordered_map<K, V, Hash, KeyEqual>& m) { for (const auto& [k, v] : m) { cout << k << " : " << v << " "; } cout << '\n'; }
+template<typename T> void print_i(const T& arr) {cout << "{"; for (const auto& element: arr) {cout << element << ", ";} cout << "\b\b}\n";}
+template<typename K, typename V> void print_m(const map<K, V>& m) { for (const auto& [k, v] : m) { cout << "{" << k << " : " << v << "} "; } cout << '\n'; }
+template<typename K, typename V> void print_m(const unordered_map<K, V>& m) { for (const auto& [k, v] : m) { cout << "{" << k << " : " << v << "} "; } cout << '\n'; }
 
 int main() {
+    fast_cin();
     return 0;
 }
