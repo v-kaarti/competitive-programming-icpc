08b6b9d8d9100013bdc7f23b601ea68d903f8272
Vijaykaarti Sundarapandiyan <vijaykaarti4@gmail.com>
2023-04-26 16:12:57 -0400

    Create main_template.cpp

---
+++ main_template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+const int M = 1e9+7;
+
+inline void fast_cin() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+template<typename T>
+void print(T arr) {
+	for(auto element: arr) {
+		cout << element << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+    return 0;
+}
