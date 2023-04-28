d83b294646a1eec135d9a789e21b71c43f9c98ed
Vijaykaarti Sundarapandiyan <vijaykaarti4@gmail.com>
2023-04-28 03:02:15 -0400

    Update primality_algs.cpp

--- algorithms/primality_algs.cpp
+++ algorithms/primality_algs.cpp
@@ -1,9 +1,7 @@
 using u64 = uint64_t;
 using u128 = __uint128_t;
 
-// Fermat
-
-u64 binpow_m(u64 base, u64 e, u64 mod) {
+u64 binpow_m_u64(u64 base, u64 e, u64 mod) {
     u64 result = 1;
     base %= mod;
     while (e) {
@@ -15,7 +13,6 @@ u64 binpow_m(u64 base, u64 e, u64 mod) {
     return result;
 }
 
-/* 
 long long binpow_m(long long a, long long b, long long m) {
     a %= m;
     long long res = 1;
@@ -27,7 +24,20 @@ long long binpow_m(long long a, long long b, long long m) {
     }
     return res;
 }
-*/
+
+bool check_composite_u64(u64 n, u64 a, u64 d, int s) {
+    u64 x = binpow_m_u64(a, d, n);
+    if (x == 1 || x == n - 1)
+        return false;
+    for (int r = 1; r < s; r++) {
+        x = (u128)x * x % n;
+        if (x == n - 1)
+            return false;
+    }
+    return true;
+};
+
+// Fermat
 
 bool prime_fermat(int n, int iter = 5) {
     if (n < 4)
@@ -42,21 +52,9 @@ bool prime_fermat(int n, int iter = 5) {
 
 // Miller-Rabin
 
-bool check_composite(u64 n, u64 a, u64 d, int s) {
-    u64 x = binpow_m(a, d, n);
-    if (x == 1 || x == n - 1)
-        return false;
-    for (int r = 1; r < s; r++) {
-        x = (u128)x * x % n;
-        if (x == n - 1)
-            return false;
-    }
-    return true;
-};
-
 bool miller_rabin(
     u64 n,
-    int iter = 5) { // returns true if n is probably prime, else returns false.
+    int iter = 5) { 
     if (n < 4)
         return n == 2 || n == 3;
     int s = 0;
@@ -75,7 +73,7 @@ bool miller_rabin(
 
 // Deterministic miller-rabin
 
-bool miller_rabin(u64 n) {
+bool miller_rabin_det(u64 n) {
     if (n < 2)
         return false;
     int r = 0;
