47a019b84233647f5f51c39e71dac3ac846abae7
Vijay Sundarapandiyan <vijaykaarti4@gmail.com>
2023-06-13 14:54:16 -0400

    Update dsu.cpp

--- ds/dsu.cpp
+++ ds/dsu.cpp
@@ -1,19 +1,19 @@
 #define SZ
-vector<int> parent(SZ + 1, -1);
+vector<int> parent(SZ);
+vector<int> size(SZ, 1);
 
-void make_set(int v) {
-    parent[v] = v;
-    size[v] = 1;
+void make_set() {
+    for (int i = 0; i < SZ; ++i) {
+        parent[i] = i;
+    }
 }
 
 int find(int v) {
-    if (v == parent[v]) {
-        return v;
-    } 
-    return parent[v] = find(parent[v]);
+    if (v != parent[v]) parent[v] = find(parent[v]);
+    return parent[v];
 }
 
-int union(int a, b) {
+bool union(int a, b) {
     a = find(a);
     b = find(b);
     if (a != b) {
